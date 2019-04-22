49203ed0bf93366d238c95de2fde75b0480c5413
Arnold Gandarillas Castillo <arkgast@gmail.com>
2019-04-21 22:02:26 -0500

    feat: add compount component

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,50 @@
-import React from 'react';
+import React, { Fragment, useState } from 'react';
 
-const App = () => (
-  <h1>Form Validator Reactstrap Example</h1>
+interface IInputProps {
+  name: string,
+  value?: string,
+  error?: string,
+  onChange: () => void
+}
+
+const Input: React.SFC<IInputProps> = ({ name, value, error }) => (
+  <Fragment>
+    <input name={name} value={value} />
+    {error && <span>{error}</span>}
+  </Fragment>
 )
+
+interface IFormProps {
+  children: any
+}
+
+const Form: React.SFC<IFormProps> = ({ children }) => {
+  const [error, setError] = useState('')
+
+  const handleClick = () => {
+    setError('Input has error')
+  }
+
+  return (
+    <form>
+      {React.Children.map(children, (child) => {
+        return React.cloneElement(child, {
+          error
+        })
+      })}
+      <button type='button' onClick={handleClick}>Set error</button>
+    </form>
+  )
+}
+
+const App = () => {
+  const handleChange = () => {}
+
+  return (
+    <Form>
+      <Input name='email' value='' onChange={handleChange} />
+    </Form>
+  )
+}
 
 export default App;
